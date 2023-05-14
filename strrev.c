8f5b6b35fa22d64fcdc09f14031aafba8e8de767
22P31A0523 <119730473+22P31A0523@users.noreply.github.com>
2023-05-14 19:00:33 +0530

    Add files via upload

---
+++ strrev.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+    char str[100],str1[100];
+    scanf("%[^\n]s",&str);
+    int i,j,temp,len=0;
+	for(i=0;str[i]!='\0';i++)
+    {
+       len++;
+    }
+    i=0;
+    j=len-1;
+    while(i<j)
+    {
+    	temp=str[j];
+	 	str[j]=str[i];
+	 	str[i]=temp;
+	 	i++;
+	 	j--;
+	 }
+	 printf("%s",str);
+ }
+	
